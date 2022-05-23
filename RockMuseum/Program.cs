e5eb865fcff3047b36fab931165faa454ae51046
Durga Prasad Mishra <durg0001@edu.zealand.dk>
2022-05-23 07:55:41 +0200

    Add project files.

---
+++ RockMuseum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RockMuseum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
